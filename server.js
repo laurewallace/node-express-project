9de8a3f1a1775d0e0cad3584312a200d55ec55f7
Lauren <laurewallace@gmail.com>
2018-11-26 10:34:36 -0500

    initial commit to heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use("/", routes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
 })
